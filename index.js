aa1756f00c4480f5816c07eaa0a80fe1fc56656c
miroswald <altamirsantos93@hotmail.com>
2019-10-24 19:51:23 -0300

    Altera o return da rota

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const app = express();
 
 //rota de teste
 app.get('teste',(req,res)=>{
-    return res.json({msg:"Hello World!"})
+    //return res.json({msg:"Hello World!"})
+    //Fazendo alteração para modificar no git
+    return res.json({hello:"World!"}) 
 });
 
 app.listen(2469);
